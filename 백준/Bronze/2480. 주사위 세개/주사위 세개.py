2d95cbe5fc68524e471eaf1ef6422c0c358947b5
clov2r <118502976+clov2r@users.noreply.github.com>
2024-03-08 17:54:33 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,14 +1,14 @@
-x, y, z = map(int, input().split())  # 입력 받기 및 정수 변환을 한 줄로 간소화
+x, y, z = input().split()  
+x = int(x)  
+y = int(y)  
+z = int(z)  
 
-# 모두 같은 경우
 if x == y == z:
     print(10000 + x * 1000)
-# 두 개만 같은 경우
 elif x == y or y == z or x == z:
     if x == y or x == z:
         print(1000 + x * 100)
     else:  # y == z
         print(1000 + y * 100)
-# 모두 다른 경우
 else:
-    print(max(x, y, z) * 100)  # 가장 큰 수에 대해 계산
+    print(max(x, y, z) * 100)
