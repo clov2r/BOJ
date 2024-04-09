74563a20b5c5a52e8f453a6018da57697438729b
clov2r <118502976+clov2r@users.noreply.github.com>
2024-04-09 14:10:25 +0900

    [Bronze II] Title: 대표값2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,9 +1,12 @@
-def solution(X):
+def MID(X):
     X.sort()
-    return X[len(X)//2]
+    return(X[len(X)//2])
 X=[]
 for i in range(5):
     X.append(int(input()))
-avg=int(sum(X)/len(X))
-print(avg) # 평균값
-print(solution(X)) #중앙값
+
+#평균
+AVG=int(sum(X)/len(X))
+print(AVG)
+#중앙값
+print(MID(X))
