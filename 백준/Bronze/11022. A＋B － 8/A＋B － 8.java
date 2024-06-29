b9d9cf3b6608f33566eeb6336b989388512ba01e
clov2r <118502976+clov2r@users.noreply.github.com>
2024-06-29 17:20:25 +0900

    [Bronze V] Title: A+B - 8, Time: 152 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt(); // Test case 입력
+        for (int i = 1; i < T+1; i++) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.printf("Case #%d: %d + %d = %d\n",i, A, B, A+B);
+        }
+    }
+}
