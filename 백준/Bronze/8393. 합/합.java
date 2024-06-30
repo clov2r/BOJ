e2b1e5a529d23b75ee822aed2896b79615a91fc9
clov2r <118502976+clov2r@users.noreply.github.com>
2024-06-30 14:54:18 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 12812 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt(); // testcase
+        int res=0;
+        for(int i=1; i<T+1; i++){
+            res+=i;
+        }
+        System.out.println(res);
+    }
+}
