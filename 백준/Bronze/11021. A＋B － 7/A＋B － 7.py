b22021b5afd4f2c962c4c21acc355baa5a9c5ca7
clov2r <118502976+clov2r@users.noreply.github.com>
2024-07-11 21:04:35 +0900

    [Bronze V] Title: A+B - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,8 +1,4 @@
-import sys
-test=int(input())
-for i in range(test):
-    a, b=sys.stdin.readline().split()
-    a=int(a)
-    b=int(b)
-    print("Case #%d: " %(i+1), end="")
-    print(a+b)
+N=int(input())
+for i in range(N):
+    A, B=map(int, input().split())
+    print('Case #%d:'%(i+1), A+B)
