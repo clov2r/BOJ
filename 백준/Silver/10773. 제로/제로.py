5934800884129520d309a1e1ee0d2a5a59318e98
CHOI MYEONGJI <118502976+clov2r@users.noreply.github.com>
2024-07-15 20:17:40 +0900

    [Silver IV] Title: 제로, Time: 2668 ms, Memory: 31900 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -2,7 +2,7 @@ K=int(input()) #정수 K
 K_list=[] #K개의 수를 받을 리스트 생성
 for i in range(K):
     k=int(input())
-    if k==0:
+    if k==0: 
         K_list.pop() #0일 경우 최근에 쓴 수 삭제
     else:
         K_list.append(k) #0이 아니면 수 더함
