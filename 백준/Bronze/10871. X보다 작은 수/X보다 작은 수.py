3dc09053c2a57b1c712fa37db828398e096ead46
CHOI MYEONGJI <118502976+clov2r@users.noreply.github.com>
2024-07-16 21:19:30 +0900

    [Bronze V] Title: X보다 작은 수, Time: 40 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,7 +1,7 @@
-n, x= input().split()
-n=int(n)
-x=int(x)
-n_list = list(map(int, input().split()))
-for num in n_list:
-    if(num<x):
-        print(num, end=' ')
+N, X =map(int, input().split()) #N개의 수열 X보다 작은 수 출력
+N_list=list(map(int, input().split())) #N개의 수열 생성
+res=[] #N_list 안에 있는 요소들 중 X보다 작은 수만 저장하는 배열
+for i in range(len(N_list)):
+    if N_list[i]<X:
+        res.append(N_list[i])
+print(*res)
