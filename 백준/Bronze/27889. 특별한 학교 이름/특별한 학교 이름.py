2291cd82ba28e4c7d37cfa91d6f055f8f14d5c5a
CHOI MYEONGJI <118502976+clov2r@users.noreply.github.com>
2024-07-23 17:43:33 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+X=input()
+if X=='NLCS':
+    print("North London Collegiate School")
+elif X=='BHA':
+    print("Branksome Hall Asia")
+elif X=='KIS':
+    print("Korea International School")
+elif X=='SJA':
+    print("St. Johnsbury Academy")
